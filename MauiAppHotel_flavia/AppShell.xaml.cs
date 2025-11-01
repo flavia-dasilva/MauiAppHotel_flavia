6a8e33bc8c2cb8e08a1247527a4c13ae0b3916ff
Flávia da Silva <oiflavissima@gmail.com>
2025-11-01 13:06:29 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel_flavia/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppHotel_flavia
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiAppHotel_flavia/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppHotel_flavia
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
